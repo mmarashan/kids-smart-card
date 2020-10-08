0ae93d11aca0eb09205514bc3b269e9970b5c5f2
Maxim <maxim@ubuntu.ubuntu-domain>
2020-10-09 00:14:23 +0300

    load category and save to db

---
+++ core/article-data/src/main/java/ru/volgadev/article_data/storage/ArticleCategoriesDataBase.kt
@@ -0,0 +1,52 @@
+package ru.volgadev.article_data.storage
+
+import android.content.Context
+import androidx.annotation.WorkerThread
+import androidx.room.*
+import ru.volgadev.article_data.model.Article
+import ru.volgadev.article_data.model.ArticleType
+import java.util.stream.Collectors
+
+@Dao
+@WorkerThread
+interface ArticleChannelsDao {
+    @Query("SELECT * FROM article")
+    fun getAll(): List<Article>
+
+    @Query("SELECT * FROM article WHERE id IN (:userIds)")
+    fun loadAllByIds(userIds: IntArray): List<Article>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(vararg users: Article)
+
+    @Delete
+    fun delete(user: Article)
+
+    @Query("SELECT EXISTS(SELECT * FROM article WHERE id = :id)")
+    fun isRowIsExist(id : Int) : Boolean
+}
+
+@Database(entities = [Article::class], version = 1)
+@TypeConverters(ListStringConverter::class, ArticleTypeConverter::class)
+abstract class ArticleChannelsDatabase : RoomDatabase() {
+
+    abstract fun userDao(): ArticleDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: ArticleDatabase? = null
+
+        private const val ARTICLE_DATABASE_NAME = "article-categories-database.db"
+
+        fun getInstance(context: Context): ArticleDatabase =
+            INSTANCE ?: synchronized(this) {
+                INSTANCE ?: buildDatabase(context).also { INSTANCE = it }
+            }
+
+        private fun buildDatabase(context: Context) =
+            Room.databaseBuilder(
+                context.applicationContext,
+                ArticleDatabase::class.java, ARTICLE_DATABASE_NAME
+            ).build()
+    }
+}
