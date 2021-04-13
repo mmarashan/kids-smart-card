7412f228e8f60ea7d12167fb2f4998d434652407
Maxim <maxim@ubuntu.ubuntu-domain>
2021-04-13 22:39:38 +0300

    KIDSCARD-8 рефакторинг репозитория музыки

--- app/src/main/java/ru/volgadev/papastory/di/core/MusicRepositoryModule.kt
+++ app/src/main/java/ru/volgadev/papastory/di/core/MusicRepositoryModule.kt
@@ -4,23 +4,17 @@ import android.content.Context
 import dagger.Binds
 import dagger.Module
 import dagger.Provides
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.InternalCoroutinesApi
 import ru.volgadev.music_data.data.MusicBackendApiImpl
 import ru.volgadev.music_data.data.MusicDatabaseProvider
+import ru.volgadev.music_data.domain.MusicRepositoryImpl
 import ru.volgadev.music_data.domain.MusicBackendApi
 import ru.volgadev.music_data.domain.MusicRepository
-import ru.volgadev.music_data.domain.MusicRepositoryImpl
 import ru.volgadev.music_data.domain.MusicTrackDatabase
 
 @Module
 interface MusicRepositoryModule {
     companion object {
-
-        @Provides
-        fun providesIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
-
         @Provides
         fun providesMusicTrackDatabase(context: Context): MusicTrackDatabase =
             MusicDatabaseProvider.createDatabase(context)
